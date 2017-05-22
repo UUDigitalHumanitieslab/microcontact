002995b6395a02869f13844c20fd6d9b032f9cf1
BeritJanssen <berit.janssen@meertens.knaw.nl>
2017-05-22 16:06:26 +0200

    Changed hash function and using now Counter block cipher instead of Cipher Block Chaining cipher

--- microcontact/encryption_test.py
+++ microcontact/encryption_test.py
@@ -1,16 +1,18 @@
 from . import encryption
 import filecmp
-from os import urandom, getcwd
+from os import urandom
+from Crypto.Random import random
 
 def test_encryption():
 	# loads test audio file, encrypts and decrypts it, and compares original and encrypted file
-	test_key = urandom(12)
+	#test_password = "ok testing"
+	test_password = urandom(12)
 	with open('microcontact/testcases/mingus.mp3', 'rb') as in_file,\
 	 open('microcontact/testcases/mingus.enc', 'wb') as enc_file:
-		encryption.encrypt(in_file, enc_file, test_key)
+		encryption.encrypt(in_file, enc_file, test_password)
 	with open('microcontact/testcases/mingus.enc', 'rb') as enc_file,\
 	 open('microcontact/testcases/mingusnew.mp3', 'wb') as dec_file:
-		encryption.decrypt(enc_file, dec_file, test_key)
+		encryption.decrypt(enc_file, dec_file, test_password)
 	assert filecmp.cmp('microcontact/testcases/mingus.mp3', 
 	 'microcontact/testcases/mingusnew.mp3', False)
 
