b897bd64abd678f4ada90f3b717ed86598c2a1ec
JelmerVNuss <jelmervannuss@gmail.com>
2017-07-06 17:00:36 +0200

    Add Language model and update Recording model

--- recordings/models.py
+++ recordings/models.py
@@ -9,7 +9,11 @@ class Dialect(models.Model):
     dialect = models.CharField(max_length=200)
 
 
+class Language(models.Model):
+    language = models.CharField(max_length=200)
+
 class Recording(models.Model):
+    id = models.AutoField(primary_key=True)
     status_choices = (
         ('a', 'censored'),
         ('b', 'reviewed'),
@@ -29,7 +33,15 @@ class Recording(models.Model):
     )
     sex = models.CharField(max_length=1, choices=sex_choices)
     age = models.IntegerField()
+    languages = models.ForeignKey(Language, on_delete="PROTECT")
     dialect = models.ForeignKey(Dialect, on_delete="PROTECT")
+    is_public_recording = models.BooleanField(default=False)
+    speaker_generation_choices = (
+        ('a', 'first')
+        ('b', 'second')
+    )
+    speaker_generation = models.CharField(max_length=1, choices=speaker_generation_choices)
     city_coordinates_lon = models.FloatField()
     city_coordinates_lng = models.FloatField()
+    year_migrated_to_americas = models.DateField(null=True, blank=True)
     recording_link = models.CharField(max_length=200)
