646780cb15b747333eb301c2d48626c3a0f5af0e
Julian Gonggrijp <j.gonggrijp@uu.nl>
2017-07-18 11:36:02 +0200

    Add a blank line per PEP 8

--- recordings/serializers.py
+++ recordings/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import Dialect
 
+
 class DialectSerializer(serializers.ModelSerializer):
     """Serializer to map the Model instance into JSON format."""
 
