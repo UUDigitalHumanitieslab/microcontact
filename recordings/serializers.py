04ce80d345a99453a12f9f3611e1c1768b6dd74e
Julian Gonggrijp <j.gonggrijp@uu.nl>
2017-07-18 17:47:26 +0200

    Add preliminary serializers, views and URLs (#40)
    This is not the full story yet, but it is a step towards enabling
    uploads.

--- recordings/serializers.py
+++ recordings/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Dialect
+from .models import Dialect, Country, Place, Recording
 
 
 class DialectSerializer(serializers.ModelSerializer):
@@ -9,3 +9,27 @@ class DialectSerializer(serializers.ModelSerializer):
         """Meta class to map serializer's fields with the model fields."""
         model = Dialect
         fields = ('id', 'dialect')
+
+
+class CountrySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Country
+        fields = ('id', 'name')
+
+
+class PlaceSerializer(serializers.ModelSerializer):
+    country = serializers.StringRelatedField()
+
+    class Meta:
+        model = Place
+        fields = ('id', 'placeID', 'name', 'latitude', 'longitude', 'country')
+
+    def to_internal_value(self, data):
+        """ Permit parsing of objects from the Google Maps API. """
+        return super().to_internal_value(data)
+
+
+class RecordingSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta():
+        model = Recording
+        fields = ('url', 'recording', 'dialect', 'place')
