11a8a9e44c0ab4ecefbb85fa99ceceb4a7d82c96
Julian Gonggrijp <j.gonggrijp@uu.nl>
2017-07-19 16:29:11 +0200

    Restrict the visible fields in the Recording admin
    For now. Closes #6, also related to #13.

--- recordings/admin.py
+++ recordings/admin.py
@@ -5,8 +5,20 @@ from django.contrib import admin
 
 from .models import Dialect, Recording, Language, Place, Country
 
+
+class RecordingAdmin(admin.ModelAdmin):
+    """ Customizations to the default ModelAdmin. """
+    fields = (
+        'status',
+        'dialect',
+        'is_public_recording',
+        'place',
+        'recording',
+    )
+
+
 admin.site.register(Dialect)
-admin.site.register(Recording)
+admin.site.register(Recording, RecordingAdmin)
 admin.site.register(Language)
 admin.site.register(Place)
 admin.site.register(Country)
