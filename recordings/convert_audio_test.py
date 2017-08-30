082f153595988c54de1c963c8cfabc91811414df
Julian Gonggrijp <j.gonggrijp@uu.nl>
2017-08-30 10:49:42 +0200

    Move microcontact/testcases/ to recordings/testcases/

--- recordings/convert_audio_test.py
+++ recordings/convert_audio_test.py
@@ -1,7 +1,13 @@
 from . import convert_audio
 import os
+import os.path
+
+BASE = os.path.join('recordings', 'testcases')
+INFILE = 'speech.amr'
+OUTFILE = 'speech.mp3'
+
 
 def test_conversion():
-	infile = "microcontact/testcases/speech.amr"
-	convert_audio.convert_to_mp3(infile)
-	assert("speech.mp3" in os.listdir("microcontact/testcases"))
+    infile = os.path.join(BASE, INFILE)
+    convert_audio.convert_to_mp3(infile)
+    assert(OUTFILE in os.listdir(BASE))
