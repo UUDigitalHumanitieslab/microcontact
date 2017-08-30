15f748cefe1f7ed187df046a9a336470babbfe00
Julian Gonggrijp <j.gonggrijp@uu.nl>
2017-08-30 14:39:38 +0200

    Add recordings.utils.get_file_size

--- recordings/utils.py
+++ recordings/utils.py
@@ -10,3 +10,22 @@ def get_absolute_path(file):
     if isinstance(file, File):
         return op.abspath(op.join(settings.MEDIA_ROOT, file.name))
     return op.abspath(file)
+
+
+def get_file_size(file):
+    """ Return the file size in bytes for a path or a file-like object. """
+    try:
+        # the clean way
+        return op.getsize(get_absolute_path(file))
+    except:
+        if hasattr(file, 'seek') and hasattr(file, 'tell'):
+            # file-like object without a name
+            # algo taken from https://stackoverflow.com/a/19079887
+            old_position = file.tell()
+            file.seek(0, os.SEEK_END)
+            size = file.tell()
+            file.seek(old_position, os.SEEK_SET)
+            return size
+        else:
+            # try-branch failed for a different reason
+            raise
--- recordings/utils_test.py
+++ recordings/utils_test.py
@@ -1,11 +1,15 @@
-from tempfile import NamedTemporaryFile
+from tempfile import TemporaryFile, NamedTemporaryFile
 import os.path as op
+import os
 
 from django.test import TestCase
 from django.core.files import File
 from django.conf import settings
 
 from .utils import *
+
+TEST_FILE = op.join('recordings', 'testcases', 'speech.amr')
+TEST_FILE_SIZE = 8838
 
 
 class GetAbsolutePathTestCase(TestCase):
@@ -24,3 +28,26 @@ class GetAbsolutePathTestCase(TestCase):
             absolute_path = get_absolute_path(django_file)
             assert op.isabs(absolute_path)
             assert op.isfile(absolute_path)
+
+
+class GetFileSizeTestCase(TestCase):
+    """ Using a unittest-style TestCase because of Django settings. """
+    
+    def setUp(self):
+        self.full_path = op.join(settings.BASE_DIR, TEST_FILE)
+    
+    def test_get_from_string(self):
+        assert get_file_size(self.full_path) == TEST_FILE_SIZE
+    
+    def test_get_from_named_object(self):
+        file_object = File(open(self.full_path))
+        assert get_file_size(file_object) == TEST_FILE_SIZE
+    
+    def test_get_from_unnamed_object(self):
+        data = b'123456789'
+        seek_pos = 2
+        with TemporaryFile() as temp:
+            temp.write(data)
+            temp.seek(seek_pos, os.SEEK_SET)
+            assert get_file_size(temp) == len(data)
+            assert temp.tell() == seek_pos
