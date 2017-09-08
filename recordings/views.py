31428a9e6c9698a3f193c7764bbb5207aa9a32c5
Julian Gonggrijp <j.gonggrijp@gmail.com>
2017-09-08 16:47:49 +0200

    Restrict the viewsets to read-only or read/create
    The same comment as in e7c6af2 applies here.

--- recordings/views.py
+++ recordings/views.py
@@ -1,30 +1,49 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, mixins
 from .serializers import *
 from .models import Dialect, Language, Country, AgeCategory, Recording
-# Create your views here.
 
 
-class DialectViewSet(viewsets.ModelViewSet):
+class CreateReadModelViewSet(
+        mixins.CreateModelMixin,
+        mixins.ListModelMixin,
+        mixins.RetrieveModelMixin,
+        viewsets.GenericViewSet,
+    ):
+    """
+    A viewset that provides `retrieve`, `create`, and `list` actions.
+
+    Importantly, this class does not provide `update`, `partial_update`
+    or `destroy`.
+    To use it, override the class and set the `.queryset` and
+    `.serializer_class` attributes.
+    
+    This class was copied from the example over here:
+    http://www.django-rest-framework.org/api-guide/viewsets/#custom-viewset-base-classes
+    """
+    pass
+
+
+class DialectViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Dialect.objects.all()
     serializer_class = DialectSerializer
 
 
-class LanguageViewSet(viewsets.ModelViewSet):
+class LanguageViewSet(CreateReadModelViewSet):
     queryset = Language.objects.all()
     serializer_class = LanguageSerializer
 
 
-class CountryViewSet(viewsets.ModelViewSet):
+class CountryViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Country.objects.all()
     serializer_class = CountrySerializer
 
 
-class AgeCategoryViewSet(viewsets.ModelViewSet):
+class AgeCategoryViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = AgeCategory.objects.all()
     serializer_class = AgeCategorySerializer
 
 
-class RecordingViewSet(viewsets.ModelViewSet):
+class RecordingViewSet(CreateReadModelViewSet):
     queryset = Recording.objects.filter(public=True)
     serializer_class = RecordingSerializer
