ebcc5a9103db41f19f283efef46534ccb75cca26
Julian Gonggrijp <j.gonggrijp@uu.nl>
2018-01-31 17:29:32 +0100

    Oops, merge the migrations

---
+++ recordings/migrations/0021_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('recordings', '0019_auto_20180116_1105'),
+        ('recordings', '0020_auto_20180131_1043'),
+    ]
+
+    operations = [
+    ]
